6734dfc225cde8806bc880797073fbc24a7e73a4
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2021-01-28 11:15:13 +0700

    builder with singular (lombok)

--- src/main/java/com/blibli/belajar/design/patterns/builder/BuilderApplication.java
+++ src/main/java/com/blibli/belajar/design/patterns/builder/BuilderApplication.java
@@ -1,9 +1,6 @@
 package com.blibli.belajar.design.patterns.builder;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -20,6 +17,7 @@ public class BuilderApplication {
         private String nama;
         private String alamat;
         private Date tanggalLahir;
+        @Singular("hobi")
         private List<String> hobi;
     }
 
@@ -36,7 +34,8 @@ public class BuilderApplication {
                 .nama("Emmanuel")
                 .alamat("Indonesia")
                 .tanggalLahir(new Date())
-                .hobi(Arrays.asList("Game", "Coding"))
+                .hobi("Game")
+                .hobi("Coding")
                 .build();
 
         System.out.println(mahasiswa1);
