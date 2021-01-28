f19d025aeead32f43e45adab5f2f63a54e2734a5
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2021-01-28 14:28:55 +0700

    chain implementation (http middleware)

---
+++ src/main/java/com/blibli/belajar/design/patterns/chain/ChainApplication.java
@@ -0,0 +1,81 @@
+package com.blibli.belajar.design.patterns.chain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ChainApplication {
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Contoh {
+        private String name;
+    }
+
+    @SpringBootApplication
+    public static class Application {
+
+        @RestController
+        public class HelloController {
+
+            @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+            public Contoh helloWorld() {
+                return Contoh.builder().name("Hello, world!").build();
+            }
+        }
+
+        public static class PoweredByInterceptor implements HandlerInterceptor {
+
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                response.setHeader("X-Powered-By", "Blibli Future Program");
+                return true;
+            }
+        }
+
+        public static class AuthInterceptor implements HandlerInterceptor {
+
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                String xApi = request.getHeader("X-API");
+                if (StringUtils.hasText(xApi)) {
+                    return true;
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return false;
+                }
+            }
+        }
+
+        @Component
+        public static class WebConfiguration implements WebMvcConfigurer {
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new PoweredByInterceptor()).order(2);
+                registry.addInterceptor(new AuthInterceptor()).order(1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
